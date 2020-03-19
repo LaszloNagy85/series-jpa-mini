351671fb88ffba76322c60dfb80025769b4bb6f4
LaszloNagy85 <n.laszlo.uk@gmail.com>
2020-03-19 22:38:09 +0100

    refactor

--- src/main/java/com/codecool/seriesminiproject/SeriesMiniProjectApplication.java
+++ src/main/java/com/codecool/seriesminiproject/SeriesMiniProjectApplication.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Profile;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -106,6 +107,8 @@ public class SeriesMiniProjectApplication {
             season2.setSeries(friends);
 
             seriesRepository.save(friends);
+            List<Series> test = seriesRepository.findAll();
+            System.out.println(test);
         };
     }
 }
